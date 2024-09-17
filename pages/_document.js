1013fedf2be001d6bb02f4bace696ad64fefd85d
Manuel Flores <speed_flores24@hotmail.com>
2024-09-16 19:25:13 -0600

    add icon

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,14 @@
+import SvgDEV from "@/svg/SvgDEV";
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>DEV community</title>
+        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+      </Head>
+
       <body className="antialiased bg-[#ebebeb]">
         <Main />
         <NextScript />
